677691a9575c62dd327b930bf7a8b8db9a208383
PrashantKr13 <prashant.kr1305@gmail.com>
2023-07-31 14:35:05 +0530

    final commit

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1, // Number of slides visible at a time
+  spaceBetween: 10, // Space between slides (in pixels)
+  loop: true, // Infinite loop
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
